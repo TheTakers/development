6a23d0fc9c9bfda4c5512fa14292215722c60ca1
ningzuokun <ningzuokun@ppmoney.com>
2016-06-01 17:59:24 +0800

    首页导航动态化设置

--- development-search/src/main/resources/public/static/js/app.js
+++ development-search/src/main/resources/public/static/js/app.js
@@ -8,6 +8,39 @@ app.config(['$routeProvider', '$httpProvider', function ($route) {
         .when('/console/memory', {templateUrl: '/console/memory', controller: 'MemoryContextCtrl'});
 }]);
 
+//menu data repository
+app.factory('menuRepository',function(){
+	
+	return  {
+	 nav:[ 
+	              {id:'',name:'首页',icon:'home icon',url:'#/',pid:''},
+	              {id:'',name:'控制台',icon:'database icon',url:'',pid:''},
+	              {id:'',name:'系统设置',icon:'settings icon',url:'',pid:''}
+	              ],
+		   menu:[
+	                {id:'',name:'系统菜单',icon:'',url:'',pid:'',child:[
+	                                                                {id:'',name:'图标管理',icon:'flag icon',url:'',pid:''},
+	                                                                {id:'',name:'菜单配置',icon:'block layout icon',url:'',pid:''}
+	                                                                ]},
+	                {id:'',name:'查询配置',icon:'',url:'',pid:'',child:[
+	                                                                {id:'',name:'SQL',icon:'unhide icon',url:'',pid:''},
+	                                                                {id:'',name:'SQL组',icon:'unordered list icon',url:'',pid:''},
+	                                                                {id:'',name:'数据源',icon:'database icon',url:'',pid:''}
+	                                                                ]},
+	                {id:'',name:'用户管理',icon:'users icon',url:'',pid:'',child:[]},
+	                {id:'',name:'控制台',icon:'',url:'',pid:'',child:[
+	                                                               {id:'',name:'系统上下文',icon:'align justify icon',url:'#/console/context',pid:''},
+	                                                                {id:'',name:'内存监控',icon:'align center icon',url:'#/console/memory',pid:''}
+	                                                               ]},
+	                {id:'',name:'日志管理',icon:'',url:'',pid:'',child:[
+	                                                                {id:'',name:'系统日志',icon:'align justify icon',url:'',pid:''},
+	                                                                {id:'',name:'接口日志',icon:'align center icon',url:'',pid:''}
+	                                                                ]}
+	                ]
+	};
+});
+
+/**
 app.controller('NavCtrl', ['$scope', function ($scope) {
     $scope.$root.$on('nav:head', function (event, header) {
         $scope.header = header;
@@ -18,4 +51,13 @@ app.controller('NavCtrl', ['$scope', function ($scope) {
     $scope.is = function (header) {
         return $scope.header === header;
     };
-}]);
+}]);**/
+
+app.controller('NavCtrl', function($scope,menuRepository) {
+	$scope.nav = menuRepository.nav;
+});
+
+app.controller('MenuCtrl', function($scope,menuRepository) {
+	$scope.menu = menuRepository.menu;
+	$scope.flag = true;
+});
