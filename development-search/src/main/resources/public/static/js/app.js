5f57e66a6b94056b76202f3f54f36f06e80dbd54
ningzuokun <ningzuokun@ppmoney.com>
2016-06-03 19:58:19 +0800

    333

--- development-search/src/main/resources/public/static/js/app.js
+++ development-search/src/main/resources/public/static/js/app.js
@@ -26,26 +26,26 @@ app.factory('menuRepository',function(){
 		getMenuData:function(){
 			if(this.navId != 1){
 				return [
-				          {id:'1',name:'首页',icon:'icon-home',url:'#/',pid:''},
-			              {id:'2',name:'控制台',icon:'icon-pencil',url:'',pid:'',child:[
-			   		                                                               {id:'',name:'系统上下文',icon:'icon-edit',url:'#/console/context',pid:''},
-					                                                                {id:'',name:'内存监控',icon:'icon-remove',url:'#/console/memory',pid:''}
+				          {id:'1',name:'首页',icon:'glyphicon glyphicon-asterisk',url:'#/',pid:''},
+			              {id:'2',name:'控制台',icon:'glyphicon glyphicon-book',url:'',pid:'',child:[
+			   		                                                               {id:'',name:'系统上下文',icon:'glyphicon glyphicon-bookmark',url:'#/console/context',pid:''},
+					                                                                {id:'',name:'内存监控',icon:'glyphicon glyphicon-print',url:'#/console/memory',pid:''}
 					                                                               ]},
-			              {id:'3',name:'系统设置',icon:'icon-folder-open',url:'',pid:'',child:[
+			              {id:'3',name:'系统设置',icon:'glyphicon glyphicon-camera',url:'',pid:'',child:[
 				        
-		                {id:'',name:'系统菜单',icon:'icon-edit',url:'',pid:'',child:[
-		                                                                {id:'',name:'图标管理',icon:'icon-user',url:'',pid:''},
-		                                                                {id:'',name:'菜单配置',icon:'icon-external-link',url:'',pid:''}
+		                {id:'',name:'系统菜单',icon:'glyphicon glyphicon-align-center',url:'',pid:'',child:[
+		                                                                {id:'',name:'图标管理',icon:'glyphicon glyphicon-adjust',url:'',pid:''},
+		                                                                {id:'',name:'菜单配置',icon:'glyphicon glyphicon-tint',url:'',pid:''}
 		                                                                ]},
-		                {id:'',name:'查询配置',icon:'icon-pencil',url:'',pid:'',child:[
-		                                                                {id:'',name:'SQL',icon:'icon-bell',url:'#/search/sqlDefine',pid:''},
-		                                                                {id:'',name:'SQL组',icon:'icon-globe',url:'',pid:''},
-		                                                                {id:'',name:'数据源',icon:'icon-pencil',url:'',pid:''}
+		                {id:'',name:'查询配置',icon:'glyphicon glyphicon-backward',url:'',pid:'',child:[
+		                                                                {id:'',name:'SQL',icon:'glyphicon glyphicon-chevron-left',url:'#/search/sqlDefine',pid:''},
+		                                                                {id:'',name:'SQL组',icon:'glyphicon glyphicon-text-width',url:'',pid:''},
+		                                                                {id:'',name:'数据源',icon:'glyphicon glyphicon-tag',url:'',pid:''}
 		                                                                ]},
-		                {id:'',name:'用户管理',icon:'icon-edit',url:'',pid:'',child:[]},
-		                {id:'',name:'日志管理',icon:'icon-globe',url:'',pid:'',child:[
-		                                                                {id:'',name:'系统日志',icon:'icon-folder-open',url:'',pid:''},
-		                                                                {id:'',name:'接口日志',icon:'icon-folder-open',url:'',pid:''}
+		                {id:'',name:'用户管理',icon:'glyphicon glyphicon-th-large',url:'',pid:'',child:[]},
+		                {id:'',name:'日志管理',icon:'glyphicon glyphicon-th-list',url:'',pid:'',child:[
+		                                                                {id:'',name:'系统日志',icon:'glyphicon glyphicon-user',url:'',pid:''},
+		                                                                {id:'',name:'接口日志',icon:'glyphicon glyphicon-music',url:'',pid:''}
 		                                                                ]}
 		                          ]}
 		                ];
