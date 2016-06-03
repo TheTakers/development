800e777037c3d1b249d9647b5a69902a0029f883
ningzuokun <ningzuokun@ppmoney.com>
2016-06-03 18:18:01 +0800

    菜单修改

--- development-search/src/main/resources/public/static/js/app.js
+++ development-search/src/main/resources/public/static/js/app.js
@@ -16,45 +16,38 @@ app.factory('menuRepository',function(){
 	return {
 		
 		//导航标识
-		navId : 1,
+		navId : 2,
 		
 		// 设置导航标识
 		setNavId:function(navId){
 			this.navId = navId;
 		},
-		
-		//根据导航标识获取菜单
-		getNavData:function(){
-			
-			return [ 
-		              {id:'1',name:'首页',icon:'home icon',url:'#/',pid:''},
-		              {id:'2',name:'控制台',icon:'database icon',url:'',pid:''},
-		              {id:'3',name:'系统设置',icon:'settings icon',url:'',pid:''}
-		              ];
-		},
-		
 		//获取导航数据
 		getMenuData:function(){
 			if(this.navId != 1){
 				return [
-		                {id:'',name:'系统菜单',icon:'',url:'',pid:'',child:[
-		                                                                {id:'',name:'图标管理',icon:'flag icon',url:'',pid:''},
-		                                                                {id:'',name:'菜单配置',icon:'block layout icon',url:'',pid:''}
+				          {id:'1',name:'首页',icon:'icon-home',url:'#/',pid:''},
+			              {id:'2',name:'控制台',icon:'icon-pencil',url:'',pid:'',child:[
+			   		                                                               {id:'',name:'系统上下文',icon:'icon-edit',url:'#/console/context',pid:''},
+					                                                                {id:'',name:'内存监控',icon:'icon-remove',url:'#/console/memory',pid:''}
+					                                                               ]},
+			              {id:'3',name:'系统设置',icon:'icon-folder-open',url:'',pid:'',child:[
+				        
+		                {id:'',name:'系统菜单',icon:'icon-edit',url:'',pid:'',child:[
+		                                                                {id:'',name:'图标管理',icon:'icon-user',url:'',pid:''},
+		                                                                {id:'',name:'菜单配置',icon:'icon-external-link',url:'',pid:''}
 		                                                                ]},
-		                {id:'',name:'查询配置',icon:'',url:'',pid:'',child:[
-		                                                                {id:'',name:'SQL',icon:'unhide icon',url:'#/search/sqlDefine',pid:''},
-		                                                                {id:'',name:'SQL组',icon:'unordered list icon',url:'',pid:''},
-		                                                                {id:'',name:'数据源',icon:'database icon',url:'',pid:''}
+		                {id:'',name:'查询配置',icon:'icon-pencil',url:'',pid:'',child:[
+		                                                                {id:'',name:'SQL',icon:'icon-bell',url:'#/search/sqlDefine',pid:''},
+		                                                                {id:'',name:'SQL组',icon:'icon-globe',url:'',pid:''},
+		                                                                {id:'',name:'数据源',icon:'icon-pencil',url:'',pid:''}
 		                                                                ]},
-		                {id:'',name:'用户管理',icon:'users icon',url:'',pid:'',child:[]},
-		                {id:'',name:'控制台',icon:'',url:'',pid:'',child:[
-		                                                               {id:'',name:'系统上下文',icon:'align justify icon',url:'#/console/context',pid:''},
-		                                                                {id:'',name:'内存监控',icon:'align center icon',url:'#/console/memory',pid:''}
-		                                                               ]},
-		                {id:'',name:'日志管理',icon:'',url:'',pid:'',child:[
-		                                                                {id:'',name:'系统日志',icon:'align justify icon',url:'',pid:''},
-		                                                                {id:'',name:'接口日志',icon:'align center icon',url:'',pid:''}
+		                {id:'',name:'用户管理',icon:'icon-edit',url:'',pid:'',child:[]},
+		                {id:'',name:'日志管理',icon:'icon-globe',url:'',pid:'',child:[
+		                                                                {id:'',name:'系统日志',icon:'icon-folder-open',url:'',pid:''},
+		                                                                {id:'',name:'接口日志',icon:'icon-folder-open',url:'',pid:''}
 		                                                                ]}
+		                          ]}
 		                ];
 			}
 		}
@@ -75,11 +68,13 @@ app.controller('NavCtrl', ['$scope', function ($scope) {
 }]);**/
 
 app.controller('indexCtrl', function($scope,$http,menuRepository) {
-	$scope.nav = menuRepository.getNavData();
+	
+});
+
+app.controller('menuCtrl', function($scope,$http,menuRepository) {
 	$scope.menu = menuRepository.getMenuData();
-	//nav click
-	$scope.setNavId = function(navId){
-		menuRepository.setNavId(navId);
-		$scope.menu = menuRepository.getMenuData();
+	$scope.selected = function(el){
+		
 	};
 });
+
