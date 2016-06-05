8f1a49cdcd5273839beeb9e2bd3d08d1720c7aea
zkning <120716625@qq.com>
2016-06-05 11:15:07 +0800

    Thymeleaf Javascript 应用

--- development-search/src/main/resources/public/static/assets/js/jquery.common.js
+++ development-search/src/main/resources/public/static/assets/js/jquery.common.js
@@ -2,18 +2,15 @@
  * Created by zkning on 2016/6/5.
  */
 
-	
+$.warning = function warning(info){
+	$.Notification.notify('warning','bottom right','警告', info);
+}
 
+$.error = function error(info){
+	$.Notification.notify('error','bottom right','系统异常', info);
+}
 
-	$.warning = function warning(info){
-		$.Notification.notify('warning','bottom right','警告', info);
-	}
-	
-	$.error = function error(info){
-		 $.Notification.notify('error','bottom right','错误', info);
-	}
-	
-	$.info =  function(info){
-		$.Notification.notify('info','bottom right','提示', info);
-	}
- 
+$.info =  function(info){
+	$.Notification.notify('info','bottom right','提示', info);
+}
+
