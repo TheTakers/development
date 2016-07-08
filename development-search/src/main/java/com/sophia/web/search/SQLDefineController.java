b85727ded06963ef75c454b698bcd32ad61e4f90
ningzuokun <ningzuokun@ppmoney.com>
2016-07-08 18:18:46 +0800

    sqldefine新增

--- development-search/src/main/java/com/sophia/web/search/SQLDefineController.java
+++ development-search/src/main/java/com/sophia/web/search/SQLDefineController.java
@@ -1,11 +1,25 @@
 package com.sophia.web.search;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sophia.api.BaseController;
 import com.sophia.service.UserService;
+import com.sophia.vo.QueryGridParam;
+import com.sophia.vo.SQLGroupParam;
+import com.sophia.web.constant.Constant;
 
 /**
  * 查询
@@ -20,5 +34,34 @@ public class SQLDefineController extends BaseController{
 	
 	public static final String module = "search/sqldefine";
 	
-	 
+	@RequestMapping("/index")
+    public ModelAndView index(HttpServletRequest request, ModelMap result) {
+        return new ModelAndView(module +"/index", result);
+    }
+	
+	@RequestMapping("/edit")
+    public ModelAndView edit(HttpServletRequest request, ModelMap result) {
+        return new ModelAndView(module + "/edit", result);
+    }
+	
+	@ResponseBody
+	@RequestMapping(value="/list",method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> list(@RequestBody @Valid QueryGridParam queryGridParam) {
+		try {
+			return responseOk(Constant.SUCCESS_MESSAGE);
+		} catch (Exception e) {
+			return responseError(Constant.FAILURE_MESSAGE, e);
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/save",method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> save(@RequestBody @Valid SQLGroupParam sqlGroupParam) {
+		try {
+			 
+			return responseOk(Constant.SUCCESS_MESSAGE);
+		} catch (Exception e) {
+			return responseError(Constant.FAILURE_MESSAGE, e);
+		}
+	}
 }
---
+++ development-search/src/main/resources/templates/search/sqldefine/index.js
@@ -0,0 +1,63 @@
+angular.module('app').controller('sqlDefineCtrl', function($scope,$http,$uibModal) {
+	
+	$scope.select = {text:"divdfa",value:"xxxx"};
+	
+	$scope.pagination = {};
+	$scope.queryparams = {};
+ 	$scope.sqlgroup = {};
+	$scope.search = function(){
+		
+		//广播分页条查询
+		$scope.$broadcast("sqldefinegrid");  
+	}
+	
+	$scope.sett = {
+			data:{
+				simpleData:{
+				enable: true, //不需要用户再把数据库中取出的 List 强行转换为复杂的 JSON 嵌套格式
+				idKey: "id",
+				pIdKey: "pId",
+				rootPId: 0}
+			},
+			callback: {
+				onClick: function(event,treeId,node,idx){
+					$scope.$broadcast("sqldefinegrid");  
+				}
+			}};
+	 
+	 $scope.openTemplate = function () {
+        
+          var modalInstance = $uibModal.open({
+             templateUrl: '/search/sqldefine/edit',
+             controller: 'sqlDefineCtrl',
+             resolve: {
+               items: function () {
+                 return $scope.items;
+               }
+             }
+         });
+         
+         modalInstance.result.then(function (selectedItem) {
+             $scope.selected = selectedItem;
+           }, function () {
+             $log.info('Modal dismissed at: ' + new Date());
+           });
+     };
+     
+    $scope.cancel = function() {
+    	 $scope.$close();
+  	};
+  	
+  	$scope.save = function() {
+  		$http.post('/search/sqldefine/save',$scope.sqlgroup).success(function(data){
+  			$scope.$broadcast("sqldefinegrid");  
+  			$scope.$close();
+  		});	
+ 	};
+  	
+ 	$scope.znodes = [{"id":1, "pId":0, "name":"SQL组"}, 
+ 	                 {"id":11, "pId":1, "name":"test11"}, 
+ 	                 {"id":12, "pId":1, "name":"test12"}, 
+ 	                 {"id":111, "pId":11, "name":"test111"}];
+	
+});
