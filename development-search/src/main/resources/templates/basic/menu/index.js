4652b49c42641f6d370b9e0bd8b4404f3dab917b
ningzuokun <ningzuokun@ppmoney.com>
2016-08-11 14:09:49 +0800

    代码重构

--- development-search/src/main/resources/templates/basic/menu/index.js
+++ development-search/src/main/resources/templates/basic/menu/index.js
@@ -1,14 +1,18 @@
 angular.module('app').controller('menuCtrl', function($scope,$http,$uibModal,$log,commonService) {
-	
-	//table展示的数据
-	$scope.dataList = {};
-	
-	//查询
-	$scope.search = function(){
+
+	$scope.grid  = {
+			
+		id:$.uuid(),
 		
-		//广播分页条查询
-		$scope.$broadcast("grid");  
+		//table展示的数据
+		dataList:{}, 
+		
+		//查询
+		search:function(){
+			$scope.$broadcast(this.id);  
+		}	
 	}
+
 
 	$scope.setting = {
 			async:{
@@ -30,43 +34,43 @@ angular.module('app').controller('menuCtrl', function($scope,$http,$uibModal,$lo
 					$scope.$broadcast("grid");  
 				}
 			}};
-	
+
 	//编辑
 	$scope.edit = function (id) {
 		edit(commonService,'/basic/menu/findById','/basic/menu/edit','editCtrl',{id:id},$scope.search);
 	};
-	
+
 	//删除
 	$scope.remove = function (id) {
 		remove(commonService,'/basic/menu/delete',{id:id},$scope.search);
 	};
-	
+
 	//查看
 	$scope.view = function(id){
-		
+
 	}
-	
-	
+
+
 	$scope.todo = function(target){
-		 switch(target){
-         case "edit":
-        	   $scope.edit();
-               break;
-         default :
-           $log.info(target);
-           break;
-       }
+		switch(target){
+		case "edit":
+			$scope.edit();
+			break;
+		default :
+			$log.info(target);
+		break;
+		}
 	}
-	
-	
+
+
 	//按钮工具栏
 	$scope.toolbar = {
-		id: "toolbar"+$.uuid(),
-		
-		buttonList:[{name:"新增",target:"edit"}],
-		
-		//查询列
-		inputList:[{label:"编号",type:"",value:""},
-		           {label:"名称",type:"",value:""}] 
+			id: "toolbar"+$.uuid(),
+
+			buttonList:[{name:"新增",target:"edit"}],
+
+			//查询列
+			inputList:[{label:"编号",type:"",value:""},
+			           {label:"名称",type:"",value:""}] 
 	};
 });
