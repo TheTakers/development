5a95991929cb636ae4fff1360ae8b1d5bc2d5725
ningzuokun <ningzuokun@ppmoney.com>
2016-08-11 12:26:55 +0800

    调整..

--- development-search/src/main/resources/templates/basic/menu/index.js
+++ development-search/src/main/resources/templates/basic/menu/index.js
@@ -1,11 +1,11 @@
 angular.module('app').controller('menuCtrl', function($scope,$http,$uibModal,$log,commonService) {
-
+	
+	//table展示的数据
 	$scope.dataList = {};
-	$scope.queryparams = {};
-
+	
+	//查询
 	$scope.search = function(){
 		
-		$.shadetab()
 		//广播分页条查询
 		$scope.$broadcast("grid");  
 	}
@@ -30,12 +30,43 @@ angular.module('app').controller('menuCtrl', function($scope,$http,$uibModal,$lo
 					$scope.$broadcast("grid");  
 				}
 			}};
-
+	
+	//编辑
 	$scope.edit = function (id) {
 		edit(commonService,'/basic/menu/findById','/basic/menu/edit','editCtrl',{id:id},$scope.search);
 	};
 	
+	//删除
 	$scope.remove = function (id) {
 		remove(commonService,'/basic/menu/delete',{id:id},$scope.search);
+	};
+	
+	//查看
+	$scope.view = function(id){
+		
 	}
+	
+	
+	$scope.todo = function(target){
+		 switch(target){
+         case "edit":
+        	   $scope.edit();
+               break;
+         default :
+           $log.info(target);
+           break;
+       }
+	}
+	
+	
+	//按钮工具栏
+	$scope.toolbar = {
+		id: "toolbar"+$.uuid(),
+		
+		buttonList:[{name:"新增",target:"edit"}],
+		
+		//查询列
+		inputList:[{label:"编号",type:"",value:""},
+		           {label:"名称",type:"",value:""}] 
+	};
 });
