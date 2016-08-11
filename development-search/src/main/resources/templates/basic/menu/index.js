c6c7f2644d080688b54b4d25dba958bfa914c7ca
ningzuokun <ningzuokun@ppmoney.com>
2016-08-11 15:04:47 +0800

    updating

--- development-search/src/main/resources/templates/basic/menu/index.js
+++ development-search/src/main/resources/templates/basic/menu/index.js
@@ -26,26 +26,30 @@ angular.module('app').controller('menuCtrl', function($scope,$http,$uibModal,$lo
 	}
 
 	//tree config
-	$scope.treeConfig = {
-			async:{
-				url:"/basic/menu/treeData",
-				type:"post",
-				contentType: "application/json",
-				enable:true
-			},
-			data:{
-				simpleData:{
-					enable: true, //不需要用户再把数据库中取出的 List 强行转换为复杂的 JSON 嵌套格式
-					idKey: "id",
-					pIdKey: "pid",
-					rootPId: 0
+	$scope.treeConfig ={
+			id:$.uuid(),
+			setting:{
+				async:{
+					url:"/basic/menu/treeData",
+					type:"post",
+					contentType: "application/json",
+					enable:true
+				},
+				data:{
+					simpleData:{
+						enable: true, //不需要用户再把数据库中取出的 List 强行转换为复杂的 JSON 嵌套格式
+						idKey: "id",
+						pIdKey: "pid",
+						rootPId: 0
+					}
+				},
+				callback: {
+					onClick: function(event,treeId,node,idx){
+						$scope.$broadcast($scope.grid.id);  
+					}
 				}
-			},
-			callback: {
-				onClick: function(event,treeId,node,idx){
-					$scope.$broadcast($scope.grid.id);  
-				}
-			}};
+			}
+	};
 
 
 	//编辑
