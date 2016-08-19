d55b9a617cc6531ba6698c2a57cab3ae56426792
ningzuokun <ningzuokun@ppmoney.com>
2016-08-19 19:45:01 +0800

    33333

--- development-search/src/main/resources/public/static/js/directive.js
+++ development-search/src/main/resources/public/static/js/directive.js
@@ -249,77 +249,12 @@ app.directive('uiselector', function($http,$log,$uibModal) {
 		link:function(scope,element,attr){
 			
 			scope.showDialog=function(){
-
+				
 				var modalInstance = $uibModal.open({
 					templateUrl: scope.url,
 					
 					//接收子页传值
 					controller: function($scope,$http,$uibModal,$log,$uibModalInstance,param) { 
-
-						$scope.grid = {
-								id:$.uuid(),
-								url:'/basic/menu/list',
-
-								//table展示的数据
-								dataList:{}, 
-
-								//查询
-								search:function(){
-									$scope.$broadcast(this.id);  
-								},
-								
-								action:"/basic/menu",
-								
-								opt:1,
-								
-								editCtrl:function($scope,$http,$uibModal,$log,$uibModalInstance,param) { //接收子页传值
-									
-									//页面数据
-									$scope.data = param;
-									
-								 	$scope.save = function() {
-								 		saveOfClose($http,'/basic/menu/save',$scope.data,$uibModalInstance);
-								 	};
-								 	
-								 	$scope.cancel = function() {
-								 		 $uibModalInstance.dismiss('cancel');
-								 	};
-								 	
-								 	
-								 	//字段列表
-								 	$scope.fieldList = [
-										            {title:"名称",field:"name",required:true,element:"text"},
-										            {title:"url",field:"link",required:false,element:"text"},
-										            {title:"图标",field:"ico",required:true,element:"text"},
-										            {title:"所属菜单",field:"pid",required:false,element:"selector",url:"/basic/menu/selector",option:{text:"pText"}},
-										            {title:"菜单路径",field:"path",required:false,element:"text"},
-										            {title:"排序",field:"idx",required:false,element:"text"},
-										            {title:"描述",field:"remark",required:false,element:"textarea"}
-										            ]
-								 	
-								},
-								
-								columnList:[
-								            {title:"名称",field:"name",display:true,exdata:"",query:true},
-								            {title:"url",field:"link",display:true,exdata:"",query:true},
-								            {title:"图标",field:"ico",display:true,exdata:"",query:true},
-								            {title:"所属菜单",field:"pText",display:true,exdata:"",query:true},
-								            {title:"菜单路径",field:"name",display:true,exdata:"",query:true},
-								            {title:"排序",field:"path",display:true,exdata:"",query:true},
-								            {title:"描述",field:"remark",display:true,exdata:"",query:true},
-								            {title:"操作",field:""}
-								            ]
-						}
-						
-						
-						$scope.toolbar = {
-								id: "toolbar"+$.uuid(),
-								
-								//查询列
-								inputList:[{label:"编号",field:"",element:"",value:"",expr:"",exdata:""},
-								           {label:"名称",field:"",element:"",value:"",expr:"",exdata:""}]
-						};
-						
 						
 						//选择器ok按钮
 						$scope.ok = function() {
