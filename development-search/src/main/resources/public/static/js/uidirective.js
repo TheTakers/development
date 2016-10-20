f4913666ac57265ec56ad8e8566fcdb105935c08
ningzuokun <ningzuokun@ppmoney.com>
2016-10-20 19:12:37 +0800

    ffffff

--- development-search/src/main/resources/public/static/js/uidirective.js
+++ development-search/src/main/resources/public/static/js/uidirective.js
@@ -351,13 +351,15 @@ app.directive('uiGenerateCode', function($http,$log,commonService) {
 			required:"="
 		},
 		template:function(element,atts){
-			return  '<input class="form-control input-sm" ng-model="data" required="{{required}}"></input>'+
+			return  '<input class="form-control input-sm {{clazz}}"  ng-model="data" required="{{required}}" ></input>'+
 			'<button type="button" class="btn btn-info waves-effect waves-light input-sm" ng-click="createCode()">生成</button>';
 		},
 		replace : false,			
 		transclude : false,
 		link:function(scope,element,attr){ 
-
+			
+			scope.clazz = scope.required ? "ng-required" : ""; 
+			
 			//生成编码
 			scope.createCode = function(){
 				var remoteUrl = _.isEmpty(scope.url) ?  "/basic/func/code" : scope.url;
