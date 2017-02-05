04a05a92dfae982b052a614697a2af38f5b4b484
ningzuokun <ningzuokun@ppmoney.com>
2017-02-05 18:43:44 +0800

    修改

--- development-framework/src/main/java/com/sophia/advice/handler/ControllerAdviceExceptionHandler.java
+++ development-framework/src/main/java/com/sophia/advice/handler/ControllerAdviceExceptionHandler.java
@@ -1,36 +1,36 @@
 package com.sophia.advice.handler;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.sophia.exception.RestException;
+import com.sophia.response.Response;
 
 /**
- * Created by Kim on 2015/9/15.
+ * @author zkning
  */
 @ControllerAdvice
 public class ControllerAdviceExceptionHandler extends ResponseEntityExceptionHandler {
 
-    protected Logger logger = LoggerFactory.getLogger(getClass());
+	protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    @ExceptionHandler(value = Exception.class)
-    public ModelAndView error(Exception ex) {
-        logger.error(ex.getMessage());
-        return new ModelAndView("error/error", "error", ex);
-    }
+	@ExceptionHandler(value = Exception.class)
+	public ModelAndView error(Exception ex) {
+		logger.error(ex.getMessage());
+		return new ModelAndView("error/error", "error", ex);
+	}
 
-    @ExceptionHandler(value = {RestException.class})
-    public final ResponseEntity<String> handleException(RestException ex, WebRequest request) {
-        logger.error(ex.getMessage());
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<Response<Object>> handleRuntimeException(RuntimeException ex,HttpServletRequest request){
+		logger.error("系统运行时异常:"+request.getRequestURL(),ex);
+		return new ResponseEntity<Response<Object>>(Response.FAILURE(ex), HttpStatus.OK);
+	}
 }
 
--- development-framework/src/main/java/com/sophia/api/BaseController.java
+++ development-framework/src/main/java/com/sophia/api/BaseController.java
@@ -5,13 +5,8 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import com.sophia.response.Response;
 
 /**
  * @author zkning
@@ -33,17 +28,4 @@ public class BaseController {
         ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         return attrs.getRequest();
     }
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Response<Object>> handleRuntimeException(RuntimeException ex,HttpServletRequest request){
-        logger.error("拦截运行时异常:"+request.getRequestURL(),ex);
-        return new ResponseEntity<Response<Object>>(Response.FAILURE(ex), HttpStatus.OK);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Response<Object>> handleException(Exception ex,HttpServletRequest request) {
-        logger.error("拦截异常:"+request.getRequestURL(),ex);
-        return new ResponseEntity<Response<Object>>(Response.FAILURE(ex), HttpStatus.OK);
-    }
-
 }
